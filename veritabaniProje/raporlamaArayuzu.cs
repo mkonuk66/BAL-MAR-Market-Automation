3eafa2343d59cb4632180cbd67b2773269c11a87
ibrahmozdemir1 <ibrahim.ozdemir@bil.omu.edu.tr>
2021-01-01 23:49:53 +0300

    Raporlama Arayüzü eklendi

---
+++ veritabaniProje/raporlamaArayuzu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace veritabaniProje
+{
+    public partial class raporlamaArayuzu : Form
+    {
+        public raporlamaArayuzu()
+        {
+            InitializeComponent();
+        }
+    }
+}
