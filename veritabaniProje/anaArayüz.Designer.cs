7c4f400d54a68a72c420644ade1092d68de76282
mkonuk66 <mkonuk66@hotmail.com>
2021-01-01 22:57:43 +0300

    Düzenleme

--- veritabaniProje/anaArayüz.Designer.cs
+++ veritabaniProje/anaArayüz.Designer.cs
@@ -1,5 +1,4 @@
-﻿
-namespace veritabaniProje
+﻿namespace veritabaniProje
 {
     partial class anaArayüz
     {
@@ -37,39 +36,43 @@ namespace veritabaniProje
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(69, 68);
+            this.button1.Location = new System.Drawing.Point(89, 62);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(288, 131);
+            this.button1.Size = new System.Drawing.Size(256, 123);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(404, 68);
+            this.button2.Location = new System.Drawing.Point(440, 62);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(301, 131);
+            this.button2.Size = new System.Drawing.Size(256, 123);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(69, 230);
+            this.button3.Location = new System.Drawing.Point(89, 242);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(288, 131);
+            this.button3.Size = new System.Drawing.Size(256, 123);
             this.button3.TabIndex = 2;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(404, 230);
+            this.button4.Location = new System.Drawing.Point(440, 242);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(301, 131);
+            this.button4.Size = new System.Drawing.Size(256, 123);
             this.button4.TabIndex = 3;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // anaArayüz
             // 
