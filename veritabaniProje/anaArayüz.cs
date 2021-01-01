651dd1ea566e0ee1b793dd4d4b16adfe5ac18fb4
mkonuk66 <mkonuk66@hotmail.com>
2021-01-01 23:25:47 +0300

    stok ekleme tamamlandı

--- veritabaniProje/anaArayüz.cs
+++ veritabaniProje/anaArayüz.cs
@@ -19,7 +19,8 @@ namespace veritabaniProje
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            stokEkle stk = new stokEkle();
+            stk.Show();
         }
 
         private void button2_Click(object sender, EventArgs e)
