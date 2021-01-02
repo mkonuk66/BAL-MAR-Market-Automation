8be833fa76c79f26320b5b44c29dbd7d1dbfa2af
Muhammet <mmhasta3@gmail.com>
2021-01-02 23:34:58 +0300

    Databse güncellendi

---
+++ veritabaniProje/Entity/tSatis.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace veritabaniProje.Entity
+{
+    class tSatis
+    {
+        [Key]
+        public int satisNo { get; set; }
+        public string satisTuru { get; set; }
+    }
+}
