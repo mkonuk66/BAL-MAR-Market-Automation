42a45039be9f04f8d6ec37b1adc8328fa5919e36
ibrahmozdemir1 <ibrahim.ozdemir@bil.omu.edu.tr>
2021-01-02 23:15:19 +0300

    Veritabanına bazı tablolar eklendi

---
+++ veritabaniProje/Migrations/202101022005354_InitialCreate.cs
@@ -0,0 +1,29 @@
+﻿namespace veritabaniProje.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.tUruns",
+                c => new
+                    {
+                        urunId = c.Int(nullable: false, identity: true),
+                        urunAdi = c.String(),
+                        miktar = c.Int(nullable: false),
+                        satisFiyat = c.Int(nullable: false),
+                        irsaliyeNo = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.urunId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.tUruns");
+        }
+    }
+}
