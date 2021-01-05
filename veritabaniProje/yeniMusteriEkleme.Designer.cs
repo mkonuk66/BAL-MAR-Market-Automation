ea6090faf9dd8b4791b344c3eb1c5c8c715d3d4e
Muhammet <mmhasta3@gmail.com>
2021-01-05 18:32:19 +0300

    Müşteri Kayıt ekranı gğncellendi

--- veritabaniProje/yeniMusteriEkleme.Designer.cs
+++ veritabaniProje/yeniMusteriEkleme.Designer.cs
@@ -45,39 +45,45 @@ namespace veritabaniProje
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label1.Location = new System.Drawing.Point(238, 45);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(318, 39);
+            this.label1.Size = new System.Drawing.Size(307, 38);
             this.label1.TabIndex = 0;
             this.label1.Text = "Yeni Müşteri Kaydı";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(68, 184);
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 179);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(36, 17);
+            this.label2.Size = new System.Drawing.Size(53, 24);
             this.label2.TabIndex = 1;
             this.label2.Text = "Adı :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(68, 235);
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(12, 230);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(59, 17);
+            this.label3.Size = new System.Drawing.Size(87, 24);
             this.label3.TabIndex = 2;
             this.label3.Text = "Soyadı :";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(68, 288);
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(12, 283);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(128, 17);
+            this.label4.Size = new System.Drawing.Size(188, 24);
             this.label4.TabIndex = 3;
             this.label4.Text = "Telefon Numarası :";
             // 
             // textBox1
             // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
             this.textBox1.Location = new System.Drawing.Point(208, 179);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(194, 22);
@@ -86,6 +92,8 @@ namespace veritabaniProje
             // 
             // textBox2
             // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox2.ForeColor = System.Drawing.SystemColors.Window;
             this.textBox2.Location = new System.Drawing.Point(208, 230);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(194, 22);
@@ -94,6 +102,8 @@ namespace veritabaniProje
             // 
             // textBox3
             // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox3.ForeColor = System.Drawing.SystemColors.Window;
             this.textBox3.Location = new System.Drawing.Point(208, 283);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(194, 22);
@@ -102,7 +112,9 @@ namespace veritabaniProje
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.BackColor = System.Drawing.Color.Gainsboro;
+            this.button1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.Sienna;
             this.button1.Location = new System.Drawing.Point(615, 364);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(148, 52);
@@ -115,6 +127,7 @@ namespace veritabaniProje
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox3);
@@ -125,7 +138,7 @@ namespace veritabaniProje
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "yeniMusteriEkleme";
-            this.Text = "yeniMusteriEkleme";
+            this.Text = "Yeni Müşteri Kaydı";
             this.ResumeLayout(false);
             this.PerformLayout();
 
